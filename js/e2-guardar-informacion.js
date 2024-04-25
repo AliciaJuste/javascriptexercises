24c6876de4676b10bbe8c8d5cb56c55c25f22ae8
Alice <aliciajuste@gmail.com>
2024-04-25 21:11:27 +0200

    Add e1 and e2

---
+++ js/e2-guardar-informacion.js
@@ -0,0 +1,31 @@
+//Nombre de variable recomendado comenzar en minúsculas y usar camel Case en vez de espacios
+//Declarar una variable
+let miNombre;
+
+//Crear una variable y luego asignar valor
+Document.write("EJEMPLO 1: INICIAR LA VARIABLE CIUDAD</br>");
+let ciudad;
+ciudad = "Malaga";
+Document.write("La variable ciudad tiene el valor: " + ciudad + "</br>");
+//Declarar variable y asignar valor en una sentencia
+Document.write("EJEMPLO 2: INICIAR LA VARIABLE EDAD</br>");
+let edad = 53;
+Document.write("La variable edad tiene el valor: " + edad + "</br>");
+edad = 54;
+Document.write("La variable edad cambia el valor: " + edad + "</br>");
+
+//TIPOS DE VARIABLES
+//Numero
+let numero = 5;
+let  numeroDec = 3.3;
+
+//String, cadenas. Pueden usarse comillas dobles o simples
+let saludo = "Buenos dias";
+let despedida = 'Que te vaya bonito';
+
+//boolean
+let estaSoleado = true;
+let esJueves = false;
+
+//Mostrar variables por pantalla
+Document.write(ciudad);
