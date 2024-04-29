3f45526eff653395d797515d797f3f3d099cd256
Alice <aliciajuste@gmail.com>
2024-04-29 13:44:45 +0200

    e2-guardar-informacion ejercicio1 saltos linea

--- js/e2-guardar-informacion.js
+++ js/e2-guardar-informacion.js
@@ -4,17 +4,17 @@ document.write("<h2>EJERCICIO 2: VARIABLES</h2>");
 let miNombre;
 
 //Crear una variable y luego asignar valor
-document.write("<h4>EJEMPLO 1: INICIAR LA VARIABLE CIUDAD</h4>");
-let ciudad;
-ciudad = "Malaga";
-document.write("La variable ciudad tiene el valor: " + ciudad + "</br></br>");
+document.write("<h5>EJEMPLO 1: INICIAR LA VARIABLE CIUDAD</h5>");
+let miCiudad;
+miCiudad = "Malaga";
+document.write("La variable ciudad tiene el valor: " + miCiudad + "</br></br>");
 
 //Declarar variable y asignar valor en una sentencia
-document.write("<h4>EJEMPLO 2: INICIAR LA VARIABLE EDAD</h4>");
-let edad = 53;
-document.write("La variable edad tiene el valor: " + edad + "</br>");
-edad = 54;
-document.write("La variable edad cambia el valor: " + edad + "</br>");
+document.write("<h5>EJEMPLO 2: INICIAR LA VARIABLE EDAD</h5>");
+let miEdad = 53;
+document.write("La variable edad tiene el valor: " + miEdad + "</br>");
+miEdad = 54;
+document.write("La variable edad cambia el valor: " + miEdad + "</br>");
 
 //TIPOS DE VARIABLES
 //Numero
@@ -30,7 +30,7 @@ document.write(saludo + " Erika! " + despedida + "</br>");
 let estaSoleado = false;
 let esJueves = true;
 document.write("Hoy esta soleado? " + estaSoleado + "</br>");
-document.write("Hoy es Jueves? " + esJueves + "</br>");
+document.write("Hoy es Jueves? " + esJueves + "</br></br>");
 
 /*Ejercicio 1: Declara variables del tipo que necesites para guardar la siguiente información sobre una persona. 
   Asignarle como valor información sobre ti y muestralo en tu página.. 
@@ -46,24 +46,25 @@ document.write("Hoy es Jueves? " + esJueves + "</br>");
     · Tienes mascota (verdadero o falso)
 */
 document.write("<h4>EJERCICIO 1: ASIGNAR VALORES A VARIABLES Y CAMBIARSELOS</h4>");
-let nombre, apellidos, edad, altura, ciudad, gusstaPlaya, gustaMonte, gustaChocolate, tienesMascota 
+let nombre, apellidos, edad, altura, ciudad, gustaPlaya, gustaMonte, gustaChocolate, tienesMascota;
 nombre = "Alicia";
 apellidos = "Juste Gilabert";
 edad = 53;
 altura = 1.645;
 ciudad = "Vilanova i la Geltrú";
-gustaPlaya = "false";
-gustaMonte = "true";
-gustaChocolate = "true";
-tienesMascota = "true";
-document.write("1. Informacion sobre : </b>" + nombre + " " + apellidos "</b>");
-document.write("   · Edad = " + edad + " años");
-document.write("   · Altura = " + altura + " cm");
-document.write("   · Ciudad = " + ciudad);
-document.write("   · ¿Te gusta la playa? = " + (gustaPlaya ? "SI" : "NO"));
-document.write("   · ¿Te gusta la montaña? = " + (gustaMonte ? "SI" : "NO"));
-document.write("   · ¿Te gusta la chocolate? = " + (gustaChocolate ? "SI" : "NO"));
-document.write("   · ¿Tienes mascota? = " + (tienesMascota ? "SI" : "NO"));
+gustaPlaya = false;
+gustaMonte = true;
+gustaChocolate = true;
+tienesMascota = true;
+
+document.writeln("1. Informacion sobre : </b>" + nombre + " " + apellidos + "</b>");
+document.writeln("   · Edad = " + edad + " años");
+document.writeln("   · Altura = " + altura + " cm");
+document.writeln("   · Ciudad = " + ciudad);
+document.writeln("   · ¿Te gusta la playa? = " + (gustaPlaya ? "SI" : "NO"));
+document.writeln("   · ¿Te gusta la montaña? = " + (gustaMonte ? "SI" : "NO"));
+document.writeln("   · ¿Te gusta el chocolate? = " + (gustaChocolate ? "SI" : "NO"));
+document.writeln("   · ¿Tienes mascota? = " + (tienesMascota ? "SI" : "NO"));
 
 //Cambiar valores y volverlos a mostrar por pantalla
 nombre = "Yerai";
@@ -75,14 +76,14 @@ gustaPlaya = "true";
 gustaMonte = "false";
 gustaChocolate = "true";
 tienesMascota = "false";
-document.write("1. Informacion sobre : </b>" + nombre + " " + apellidos "</b>");
-document.write("   · Edad = " + edad + " años");
-document.write("   · Altura = " + altura + " cm");
-document.write("   · Ciudad = " + ciudad);
-document.write("   · ¿Te gusta la playa? = " + (gustaPlaya ? "SI" : "NO"));
-document.write("   · ¿Te gusta la montaña? = " + (gustaMonte ? "SI" : "NO"));
-document.write("   · ¿Te gusta la chocolate? = " + (gustaChocolate ? "SI" : "NO"));
-document.write("   · ¿Tienes mascota? = " + (tienesMascota ? "SI" : "NO"));
+document.writeln("2. Informacion sobre : </b>" + nombre + " " + apellidos + "</b>");
+document.writeln("   · Edad = " + edad + " años");
+document.writeln("   · Altura = " + altura + " cm");
+document.writeln("   · Ciudad = " + ciudad);
+document.writeln("   · ¿Te gusta la playa? = " + (gustaPlaya ? "SI" : "NO"));
+document.writeln("   · ¿Te gusta la montaña? = " + (gustaMonte ? "SI" : "NO"));
+document.writeln("   · ¿Te gusta la chocolate? = " + (gustaChocolate ? "SI" : "NO"));
+document.writeln("   · ¿Tienes mascota? = " + (tienesMascota ? "SI" : "NO"));
 
 
 document.write("FIN!!!");
